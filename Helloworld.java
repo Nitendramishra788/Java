0438a035dbde4630d44003b20d6db37fdd9e771a
Nitendra mishra <nitendramishra788@gmail.com>
2026-03-06 22:54:42 +0530

    Added java day 1 practies code

---
+++ Helloworld.java
@@ -0,0 +1,78 @@
+// public class Helloworld {
+
+//     public static void main(String[] args) {
+//         System.out.println("hello world i am Nitendra Nit mishra");
+//     }
+
+// }
+
+// about java Data Type 
+
+// public class Helloworld {
+
+//      public static void main(String[]args){
+//         int age=21;
+//         double mark = 85.5;
+//         char grad = 'A';
+//         Boolean passed = true;
+//         System.out.println("Age"+age);
+//         System.out.println("Mark"+mark);
+//         System.out.println("Grade"+grad);
+//         System.out.println("Result"+passed);
+//      }
+// }
+
+
+// the part of Arithmetic  operator 
+
+// public class Helloworld {
+
+//     public static void main(String[]args){
+//         int a=5;
+//         int b=5;
+
+//         System.out.println("Addition"+(a+b));
+//         System.out.println("Sub"+(a-b));
+//         System.out.println("Mull"+(a*b));
+//         System.out.println("Divi"+(a/b));
+//         System.out.println("Modu"+(a%b));
+//     }
+// }
+
+
+
+// the part of Relational Oprator
+
+// public class Helloworld {
+//      public static void main(String[]args){
+//         int x=40;
+//         int y=50;
+
+//         System.out.println("Equal"+(x==y));
+//         System.out.println("Not Equal"+(x!=y));
+//         System.out.println("Greater"+(x>y));
+//         System.out.println("Less Than"+(x<y));
+//         System.out.println("Greater or equal"+(x>=y));
+//         System.out.println("Less or equal"+(x<=y));
+//      }
+      
+// }
+
+
+// the part of Logical Operators 
+
+public class Helloworld {
+
+   public static void main(String[]args){
+     int age = 20;
+    int salary=30000;
+
+    System.out.println("This && Op give ture when both  condition are True" +(age>18 && salary>20000));
+
+     System.out.println("This || Op give ture when atlist  one  condition  is True" +(age<18 || salary>20000));
+
+      System.out.println("This ! Op give ture when condition are true" +!(age>18));
+   }
+    
+}
+
